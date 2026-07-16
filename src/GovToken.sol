e2d98a48c52c584cef5aa103aff5d2a34e59a41a
Loc Giang <locbgiang@gmail.com>
2026-07-16 11:50:56 -0700

    comments for GovToken

--- src/GovToken.sol
+++ src/GovToken.sol
@@ -6,11 +6,23 @@ import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20P
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 
+/**
+ * @title GovToken
+ * @author Loc Giang
+ * @notice This is a governance token contract used to give holders voting power in a DAO
+ * this is the token that will be used with a governor contract to vote on proposals through governance
+ */
+// ERC20 - standard fungible token (transfer, balanceOf, ect)
+// ERC20Permit - adds gasless approvals via signatures (EIP-2612) - lets users approve
+// spending without an on-chain approve() tx
+// ERC20Votes - adds on-chain voting power tracking using checkpoints - required for OpenZeppelin
+// Governor contract
 contract GovToken is ERC20, ERC20Permit, ERC20Votes {
+    // set token name "My Token" and symbol. "MTK" 
+    // initializes EIP-712 domain for permit signatures
     constructor() ERC20("My Token", "MTK") ERC20Permit("My Token") {}
 
     // the following functions are overrides required by solidity
-
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
